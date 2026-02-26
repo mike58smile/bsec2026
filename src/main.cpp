332a631f3764c7a27d0f6d13184ef0df311a8fe8
Juraj Novosad <juraj.novosad@phonexia.com>
2026-02-26 18:28:34 +0100

    implement most of loginc for driving

--- src/main.cpp
+++ src/main.cpp
@@ -4,84 +4,117 @@
 #include "pinout.h"
 #include "fsm.h"
 #include "constants.h"
-#include "button.h"
 #include "engine_control.h"
 
 #include "CapacitorCharger.h"
 #include <avr/sleep.h>
+#include "TimerEvent.h"
+#include "led_controller.h"
+
+
+RGBLED_Controller statusLED(RGB_R_PIN, RGB_G_PIN, RGB_B_PIN); // RGB LED on pins 9, 10, 11
+
+void ledCallback() {
+    statusLED.callback();
+}
 
 CapacitorCharger capacitor;
-Button button(BTN_PIN);
+TimerEvent ledTimer(ledCallback, 100); // 1 second timer for LED updates
+
 
 State current_state = State::CHARGING;
 MotorController motor(MOTOR_PWM_PIN);
 
 
 void setup() {
-    // Initialize serial communication
     Serial.begin(9600);
+    
     if (!capacitor.begin()) {
         Serial.println("ERROR: Capacitor charger not found!");
-    while (1); 
+        while (1); 
     }
+    
     motor.begin();
-    button.begin();
+    statusLED.begin();
+    pinMode(LVR_PIN, INPUT);
+    ledTimer(RUN);
+    
+    // CSV header for plotting
+    Serial.println("State,Voltage,U_Wake,U_Eco,U_Survival");
 }
 
 
 void loop()
 {
-    button.update();
+    static unsigned long last_print = 0;
+    const unsigned long PRINT_INTERVAL = 500; // Print every 500ms
 
     float capVoltage = capacitor.getVoltage();
     int status = capacitor.charge(U_Cap_Max, 0.5, 4.8);
+
+    // Print data for plotting
+    if (millis() - last_print >= PRINT_INTERVAL) {
+        Serial.print(static_cast<int>(current_state));
+        Serial.print(",");
+        Serial.print(capVoltage, 2);
+        Serial.print(",");
+        Serial.print(U_Wake, 2);
+        Serial.print(",");
+        Serial.print(U_Eco, 2);
+        Serial.print(",");
+        Serial.println(U_Survival, 2);
+        last_print = millis();
+    }
 
     switch (current_state) {
         case State::CHARGING:
             if (capVoltage > U_Wake) {
                 current_state = State::WAKEUP;
             }
+            statusLED.setSteady(255, 0, 0);
             break;
         case State::WAKEUP:
             if (status == 1) {
                 current_state = State::READY;
             }
-            if (button.was_pressed()) {
+            if (digitalRead(LVR_PIN) == LOW) {
                 current_state = State::TURBO;
             }
+            statusLED.setSteady(255, 165, 0);
             break;
         case State::READY:
-            if (button.was_pressed()) {
+            if (digitalRead(LVR_PIN) == LOW) {
                 current_state = State::TURBO;
             }
+            statusLED.setSteady(0, 255, 0);
             break;
         case State::TURBO:
             if (capVoltage < U_Eco) {
                 current_state = State::ECO;
             }
             motor.eco_power();
+            statusLED.setAlternating(0, 0, 255, 255, 0, 0, 200);
             break;
         case State::ECO:
             if (capVoltage < U_Survival) {
                 current_state = State::SURVIVAL;
             }
             motor.full_power();
-            Serial.println("State: ECO");
+            statusLED.setBlinking(255, 255, 0, 200);
             break;
         case State::SURVIVAL:
             if (capVoltage < U_Wake) {
                 current_state = State::SLEEP;
             }
             motor.full_power();
-            Serial.println("State: SURVIVAL");
+            statusLED.setSteady(255, 0, 0);
             break;
         case State::SLEEP:
             motor.stop();
             set_sleep_mode(SLEEP_MODE_PWR_DOWN);
             sleep_enable();
             sleep_cpu();
-            Serial.println("State: SLEEP");
             break;
     }
-
+    delay(100); // Main loop delay to reduce CPU load
 }
--- src/pinout.h
+++ src/pinout.h
@@ -1,12 +1,14 @@
 #pragma once
 
-#define RGB_R_PIN 3
+#define RGB_R_PIN 11
 #define RGB_G_PIN 5
 #define RGB_B_PIN 6
 
-#define BTN_PIN 2
+#define LVR_PIN 2
 
 #define CUR_SENSOR_I2C_ADDR 0x40
+#define DISPLAY_I2C_ADDR 0x3C
+#define RESET_DISPLAY_PIN 7
 #define SOLAR_PWN_PIN 9
-#define MOTOR_PWM_PIN 11
+#define MOTOR_PWM_PIN 3
 #define DISPLAY_RELAY_PIN 8
