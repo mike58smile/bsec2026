e20bf2ab7f6f99a65015c0b6109d42e005503f73
Juraj Novosad <juraj.novosad@phonexia.com>
2026-02-26 16:39:24 +0100

    pinout

---
+++ src/pinout.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define RGB_R_PIN 3
+#define RGB_G_PIN 5
+#define RGB_B_PIN 6
+
+#define BTN_PIN 2
+
+#define CUR_SENSOR_I2C_ADDR 0x40
+#define SOLAR_PWN_PIN 9
+#define MOTOR_PWM_PIN 11
